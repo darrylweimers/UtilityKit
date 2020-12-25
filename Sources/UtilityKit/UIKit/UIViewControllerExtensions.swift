47339ccb7df5a23fa6d8a7c6f3ace3f66fde4996
Darryl Weimers <darrylweimers@gmail.com>
2020-12-25 17:04:51 -0500

    add dismiss keyboard function

--- Sources/UtilityKit/UIKit/UIViewControllerExtensions.swift
+++ Sources/UtilityKit/UIKit/UIViewControllerExtensions.swift
@@ -25,4 +25,15 @@ public extension UIViewController {
         view.removeFromSuperview()
         removeFromParent()
     }
+    
+    func dismissKeyboardWhenNotUse() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
 }
