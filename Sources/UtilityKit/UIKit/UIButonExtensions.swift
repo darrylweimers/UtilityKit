c7330397154fc0194b96ce071bf32e5569b4c47d
Darryl Weimers <darrylweimers@gmail.com>
2020-12-28 12:45:23 -0500

    floating button

--- Sources/UtilityKit/UIKit/UIButonExtensions.swift
+++ Sources/UtilityKit/UIKit/UIButonExtensions.swift
@@ -25,5 +25,17 @@ public extension UIButton {
         titleEdgeInsets = UIEdgeInsets(top: spacing, left: -image.size.width, bottom: -image.size.height, right: 0)
         imageEdgeInsets = UIEdgeInsets(top: -(titleSize.height + spacing), left: 0, bottom: 0, right: -titleSize.width)
     }
+    
+    
+    public func makeFloatingButtonAction(image: UIImage, tintColor: UIColor = .white, backgroundColor: UIColor = .blue) {
+            self.setImage(image, for: .normal)
+            self.tintColor = tintColor
+            self.backgroundColor = backgroundColor
 
+            // drop shadow
+            self.layer.shadowOpacity = 0.25
+            self.layer.shadowRadius = 5
+            self.layer.shadowOffset = CGSize(width: 0, height: 5) // move down by 5 points
+    }
+    
 }
